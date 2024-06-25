c0051f84a9ada8daeb878358d5a948705c560476
apurva-6265 <apurva18092003@gmail.com>
2024-06-25 20:59:44 +0530

    Changed navbar css for phone

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -12,11 +12,11 @@ const Navbar = () => {
   const menuRef = useRef();
 
   const openMenu = () => {
-    menuRef.current.style.right = '300px';
+    menuRef.current.style.right = '0';
   }
 
   const closeMenu = () => {
-    menuRef.current.style.right = '-100%';
+    menuRef.current.style.right = '-350px';
   }
 
   return (
